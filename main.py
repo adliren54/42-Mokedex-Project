d5c4140eb3ca9341e5deaff4458867842a63c15c
adliren54 <62419296+adliren54@users.noreply.github.com>
2023-04-26 06:58:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("👾 MokéBeast - The Non-Copyright Generic Beast Battle Game 👾")
+
+dictionary = {"Beast Name":name, "Type":type, "Special Move":move, "HP":hp, "MP":mp}
+
+name = input("\nInput your beast's name > ")
